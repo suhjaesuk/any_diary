4e048f7e64c5d787a7293994f57d8b259fc3b58a
syckor <syckor@naver.com>
2022-11-15 16:43:38 +0900

    add content html, css, js

---
+++ static/js/readContent.js
@@ -0,0 +1,41 @@
+
+
+$('.likeClick').click(function () {
+    let contentId = $('#contentId').val();
+    let userId = $('#userId').val();
+    let liked = $('.likeClick').text();
+    console.log('liked : '+liked)
+    let url = '';
+
+    console.log(contentId + ' ::: ' + userId)
+    if(liked == "❤"){
+        url = '/delLike';
+    }else{
+        url = '/addLike';
+    }
+
+    $.ajax({
+        type: 'POST',
+        url: url,
+        data: {userId: userId, contentId : contentId},
+        success: function (response) {
+        }
+    });
+
+    $.ajax({
+        type: 'GET',
+        url: '/searchLike',
+        data: {},
+        success: function (response) {
+            console.log(response)
+            if(response['click'] == false){
+                $('.likeClick').text('🤍')
+            }else{
+                $('.likeClick').text('❤')
+            }
+            $('#likeCount').text(response['count'])
+        }
+    });
+})
+
+
