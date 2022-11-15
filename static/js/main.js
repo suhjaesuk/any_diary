126d8aeb19d0a9fa58561383301f25936268bfe9
Ryan Kwon <dev.ryan.kwon@gmail.com>
2022-11-15 20:01:15 +0900

    Add username from jinja2

--- static/js/main.js
+++ static/js/main.js
@@ -14,14 +14,15 @@ function show_diary() {
         let contentId = rows[i]['contentId']
         let title = rows[i]['title']
         let content = rows[i]['content']
-        // let username = 
+        let username = rows[i]['username']
         let date = rows[i]['date']
         let emoticon = rows[i]['emoticon']
         let temp_html = `
         <div class="item" onclick="location.href ='/contentId=${contentId}'" >
-        <h3 class="title">${title}</h3><br>
+        <h3 class="title">${title}</h3>
+        <hr><br>
         <p class="content">${content}</p><br>
-        <p class="username">글쓴이</p>
+        <p class="username">${username}</p>
         <p class="date">${date}</p>
         <p class="emoticon">${emoticon}</p>
     </div>
