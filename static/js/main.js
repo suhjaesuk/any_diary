2c9d741c2062603a0323ea27a52d8790e5f5b8a7
suhjaesuk <suhjaesuk@gmail.com>
2022-11-17 09:58:00 +0900

    delete ajax

--- static/js/main.js
+++ static/js/main.js
@@ -1,15 +1,14 @@
-$(document).ready(function () {
+$(document).ready(function() {
   $('.gridContainer').empty()
   list_diary()
 })
 
-$.ajax({
-  type: 'GET',
-  url: 'api/username',
-  data: {},
-  async: false,
-  success: function (response) {
-    console.log(response)
-  }
-});
-
+// $.ajax({
+//   type: 'GET',
+//   url: 'api/username',
+//   data: {},
+//   async: false,
+//   success: function (response) {
+//     console.log(response)
+//   }
+// });
