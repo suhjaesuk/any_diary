0b4918855a6d669e109793cfc61c7adbd2ce7ce3
Ryan Kwon <dev.ryan.kwon@gmail.com>
2022-11-17 10:29:26 +0900

    hotfix 0.6

--- static/js/main.js
+++ static/js/main.js
@@ -2,13 +2,3 @@ $(document).ready(function () {
   $('.gridContainer').empty()
   list_diary()
 })
-
-$.ajax({
-  type: 'GET',
-  url: 'api/username',
-  data: {},
-  async: false,
-  success: function (response) {
-    console.log(response)
-  }
-});
